56c1d35e7c7a8ed20e1f85d3557857eb86c95035
InFlames_Fire <InFlames_Fire@outlook.com>
2022-11-28 18:12:45 +0300

    Selecting Screen - Initial Commit

---
+++ lib/screens/photo_selecting_screen.dart
@@ -0,0 +1,13 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
+class ImagePicker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
