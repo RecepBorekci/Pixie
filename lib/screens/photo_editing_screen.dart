08907db0f3d03bc53081dc3d27f4225ece159a19
Talhyn <karakoyunlutalha34@gmail.com>
2022-11-28 19:52:28 +0300

    Created PhotoEditingScreen class.

--- lib/screens/photo_editing_screen.dart
+++ lib/screens/photo_editing_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+
+
 class PhotoEditingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Editing Page'),
+          centerTitle: true,
+        ),
+        body: GestureDetector(
+
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("Selected photos come here"
+                  "crop widget eklenecek"
+                  "contrast widget etc etc"), Icon(Icons.edit)],
+            ),
+          ),
+        ),
+      ),
+    );
   }
-}
+  }
