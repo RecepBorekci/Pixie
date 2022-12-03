ab371d6a191695eb546b90be11d622095611fbfb
RecepBorekci <recepborekci29@gmail.com>
2022-12-03 12:18:12 +0300

    Changed it back to where it was.

--- lib/screens/photo_editing_screen.dart
+++ lib/screens/photo_editing_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:photo_editor/screens/welcome_screen.dart';
-import 'package:photo_editor/services/cut_out_pro_features.dart';
 import 'dart:io';
 
 import 'package:photo_editor/widgets/edit_image_viewmodel.dart';
@@ -15,19 +14,6 @@ class PhotoEditingScreen extends StatefulWidget {
 }
 
 class _PhotoEditingScreenState extends EditImageViewModel {
-  // var backgroundRemover;
-  //
-  // void getBackgroundRemover() async {
-  //   backgroundRemover = await CutOutProFeatures()
-  //       .removeBackground(Image.asset('temporary_image/flower.jpeg'));
-  // }
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getBackgroundRemover();
-  // }
-
   @override
   Widget build(BuildContext context) {
     Image newImage = Image.asset('temporary_image/flower.jpeg');
@@ -65,14 +51,6 @@ class _PhotoEditingScreenState extends EditImageViewModel {
             ),
             const SizedBox(
               height: 50,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // setState(() {
-                //   newImage = backgroundRemover;
-                // });
-              },
-              child: newImage,
             ),
             Container(
               height: 150,
