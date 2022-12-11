509749761ed28c1bd602b9381aab409c458b6442
RecepBorekci <recepborekci29@gmail.com>
2022-12-11 19:21:23 +0300

    Pushed my color correction branch. Organized the brackets and semicolons.

--- lib/screens/photo_editing_screen.dart
+++ lib/screens/photo_editing_screen.dart
@@ -32,7 +32,7 @@ class _PhotoEditingScreenState extends EditImageViewModel {
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Editing Page'),
         leading: IconButton(
@@ -42,153 +42,147 @@ class _PhotoEditingScreenState extends EditImageViewModel {
               }));
             },
             icon: Icon(Icons.arrow_back)),
-      actions: [
-        IconButton(
-          icon: const Icon(
-            Icons.save,
-            color: Colors.white,
-          ), onPressed: () => saveToGallery(context),
-          tooltip: 'Save Image',
-        )
-      ],
-    ),
-
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.save,
+              color: Colors.white,
+            ),
+            onPressed: () => saveToGallery(context),
+            tooltip: 'Save Image',
+          )
+        ],
+      ),
       backgroundColor: Colors.white,
-
       body: SingleChildScrollView(
         child: Screenshot(
           controller: screenshotController,
-
-        child: Column(
-          children: [
-
-            SafeArea(
-              child:
-              SizedBox(
-                height: MediaQuery.of(context).size.height*0.35,
-                child: Stack(
-                  children: [
-                    Image.file(File(widget.image.path)),
-                  ],
+          child: Column(
+            children: [
+              SafeArea(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  child: Stack(
+                    children: [
+                      Image.file(File(widget.image.path)),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-
-            // TODO: DO NOT uncomment lines below until these are added to the buttons. This is backgroundRemoval.
-            // ElevatedButton(
-            //   onPressed: () async {
-            //     Uint8List bytes =
-            //         await featuresHelper.removeBackground(widget.image.path);
-            //
-            //     Navigator.push(context, MaterialPageRoute(builder: (context) {
-            //       return ApiTestScreen(
-            //         originalImage: Image.file(File(widget.image.path)),
-            //         testImage: Image.memory(
-            //           bytes,
-            //           fit: BoxFit.cover,
-            //         ),
-            //       );
-            //     }));
-            //   },
-            //   child: Text('DO NOT Press Me!!! I am background remover'),
-            // ),
-            // TODO: Do not uncomment this button. This is face cutout.
-            // ElevatedButton(
-            //   onPressed: () async {
-            //     Uint8List bytes =
-            //         await featuresHelper.cutoutFace(widget.image.path);
-            //
-            //     Navigator.push(context, MaterialPageRoute(builder: (context) {
-            //       return ApiTestScreen(
-            //         originalImage: Image.file(File(widget.image.path)),
-            //         testImage: Image.memory(
-            //           bytes,
-            //           fit: BoxFit.cover,
-            //         ),
-            //       );
-            //     }));
-            //   },
-            //   child: Text('DO NOT Press Me!!! I am face cutout'),
-            // ),
-            // TODO: Do not uncomment this button. This is color correction.
-            ElevatedButton(
-              onPressed: () async {
-                Uint8List bytes =
-                    await featuresHelper.correctColor(widget.image.path);
-
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ApiTestScreen(
-                    originalImage: Image.file(File(widget.image.path)),
-                    testImage: Image.memory(
-                      bytes,
-                      fit: BoxFit.cover,
-                    ),
-                  );
-                }));
-              },
-              child: Text('DO NOT Press Me!!! I am color correction.'),
-            ),
-            Container(
-              height: 150,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(20.0),
-                            margin: EdgeInsets.only(left: 30),
-                            decoration: BoxDecoration(
-                              color: Colors.deepOrangeAccent,
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }),
-            ),
-          ],
-
-
-
-
-
-
-
-
-            // SizedBox(
-            //   height: 150,
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemBuilder: (context,index){
-            //       return Column(
-            //         children: [
-            //           Container(
-            //             padding: EdgeInsets.all(20.0),
-            //             margin: EdgeInsets.only(left:30),
-            //             decoration: BoxDecoration(
-            //               color: Colors.deepOrangeAccent,
-            //               borderRadius: BorderRadius.circular(15),
-            //             ),
-            //           )
-            //         ],
-            //       );
-            //     }),
-            // ),
-
-          ],
+              // TODO: DO NOT uncomment lines below until these are added to the buttons. This is backgroundRemoval.
+              // ElevatedButton(
+              //   onPressed: () async {
+              //     Uint8List bytes =
+              //         await featuresHelper.removeBackground(widget.image.path);
+              //
+              //     Navigator.push(context, MaterialPageRoute(builder: (context) {
+              //       return ApiTestScreen(
+              //         originalImage: Image.file(File(widget.image.path)),
+              //         testImage: Image.memory(
+              //           bytes,
+              //           fit: BoxFit.cover,
+              //         ),
+              //       );
+              //     }));
+              //   },
+              //   child: Text('DO NOT Press Me!!! I am background remover'),
+              // ),
+              // TODO: Do not uncomment this button. This is face cutout.
+              // ElevatedButton(
+              //   onPressed: () async {
+              //     Uint8List bytes =
+              //         await featuresHelper.cutoutFace(widget.image.path);
+              //
+              //     Navigator.push(context, MaterialPageRoute(builder: (context) {
+              //       return ApiTestScreen(
+              //         originalImage: Image.file(File(widget.image.path)),
+              //         testImage: Image.memory(
+              //           bytes,
+              //           fit: BoxFit.cover,
+              //         ),
+              //       );
+              //     }));
+              //   },
+              //   child: Text('DO NOT Press Me!!! I am face cutout'),
+              // ),
+              // TODO: Do not uncomment this button. This is color correction.
+              // ElevatedButton(
+              //   onPressed: () async {
+              //     Uint8List bytes =
+              //         await featuresHelper.correctColor(widget.image.path);
+              //
+              //     Navigator.push(context, MaterialPageRoute(builder: (context) {
+              //       return ApiTestScreen(
+              //         originalImage: Image.file(File(widget.image.path)),
+              //         testImage: Image.memory(
+              //           bytes,
+              //           fit: BoxFit.cover,
+              //         ),
+              //       );
+              //     }));
+              //   },
+              //   child: Text('DO NOT Press Me!!! I am color correction.'),
+              // ),
+              Container(
+                height: 150,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(20.0),
+                              margin: EdgeInsets.only(left: 30),
+                              decoration: BoxDecoration(
+                                color: Colors.deepOrangeAccent,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
-
-
-        ),
-  );
-
+    );
   }
-
-
-
 }
+
+// SizedBox(
+//   height: 150,
+//   child: ListView.builder(
+//     scrollDirection: Axis.horizontal,
+//     itemBuilder: (context,index){
+//       return Column(
+//         children: [
+//           Container(
+//             padding: EdgeInsets.all(20.0),
+//             margin: EdgeInsets.only(left:30),
+//             decoration: BoxDecoration(
+//               color: Colors.deepOrangeAccent,
+//               borderRadius: BorderRadius.circular(15),
+//             ),
+//           )
+//         ],
+//       );
+//     }),
+// ),
+
+//         )],
+//         ),
+//       ),
+//
+//
+//         ),
+//   );
+//
+//   }
+//
+//
+//
+// }
